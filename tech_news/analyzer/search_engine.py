0892bea051cbe99d020000f99deea1748fea79ae
rubimeryka <erykarubim13@gmail.com>
2023-09-05 14:45:54 -0300

    8 - Crie a função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -10,9 +11,18 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
-
+    try:
+        iso_format = datetime.strptime(date, "%Y-%m-%d")
+        date_in_format = iso_format.strftime("%d/%m/%Y")
+        
+        search = search_news({"timestamp": {"$regex": date_in_format}})
+        
+        list_of_news = [(news["title"], news["url"]) for news in search]
+        
+        return list_of_news
+    
+    except ValueError:
+        raise ValueError("Data inválida")
 
 # Requisito 9
 def search_by_category(category):
